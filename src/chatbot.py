1cddcc86e5c327e6b66c87a65778af87c6ef8ac6
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-02-12 13:25:04 +0800

    Update chatbot.py

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,7 +1,6 @@
 import logging
 from datetime import datetime
-# from revChatGPT.revChatGPT import Chatbot
-from pyChatGPT import ChatGPT
+from revChatGPT.Proxied import Chatbot
 
 
 class ChatBotWithExpiration:
@@ -9,21 +8,9 @@ class ChatBotWithExpiration:
     ChatGPTBot with recording last access time
     """
     def __init__(self, config) -> None:
-        self.config = config
-        self.bot = self._init_bot()
+        self.bot = Chatbot(config)
         self.last_access_time = datetime.now().timestamp()
         self.err_msg = "[Chat-Bot] 请求 ChatGPT 失败, 请重试"
-
-    def _init_bot(self) -> ChatGPT:
-        try:
-            logging.info("[ChatBot] 初始化 Chatbot...")
-            # self.bot = Chatbot(config, conversation_id=None)
-            bot = ChatGPT(**self.config)
-        except Exception as e:
-            logging.error("[ChatBot] 初始化 Chatbot 失败: %s", e)
-            bot = None
-        return bot
-
 
     def _update_last_access_time(self):
         self.last_access_time = datetime.now().timestamp()
@@ -32,8 +19,7 @@ class ChatBotWithExpiration:
         """
         reset conversation session
         """
-        # self.bot.reset_chat()
-        self.bot.reset_conversation()
+        self.bot.reset_chat()
         self._update_last_access_time()
         return "[ChatBot] ChatGPT 会话已重置, 发送消息开始聊天"
 
@@ -43,10 +29,7 @@ class ChatBotWithExpiration:
         """
         logging.info("[Chat-Bot] requesting ChatGPT: %s", text)
         try:
-            # response = self.bot.get_chat_response(text)["message"]
-            if self.bot is None:
-                self.bot = self._init_bot()
-            response = self.bot.send_message(text)["message"]
+            response = self.bot.ask(text)["message"]
         except Exception as e:
             logging.error("[Chat-Bot] Request ChatGPT failed. %s", e)
             response = self.err_msg
